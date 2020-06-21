4509d7486ad2fbef56197323b5d42b5ceac91857
vktorbr <vktorbr@gmail.com>
2020-06-21 20:00:07 -0300

    add method rollDice()

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+    }
+
+    private fun rollDice() {
+        val resultText:TextView = findViewById(R.id.result_text)
+        val random = Random.nextInt(7)
+        resultText.text = random.toString()
+
+    }
+}
